852534baa6916d8cb24baa648eaf8480295964ed
Alex Stanescu <74561825+stanescu-alex007@users.noreply.github.com>
2024-01-23 19:16:14 +0200

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,54 @@
+
+class NPCDeBuffDamage:
+    fireDamage = 10
+    poisonDamage = 8
+    starvingDamage = 5
+    #healingHealth = 100
+
+
+class NPCHealth:
+    knightHealth = 200
+    archerHealth = 100
+    pykemanHealth = 150
+    wizardHealth = 100
+
+
+class NPCDamage:
+    knightDamage = 50
+    archerDamage = 70
+    pykemanDamage = 45
+    wizardDamage = 60
+
+
+class NPCArmour:
+    knightArmour = 70
+    archerArmour = 35
+    pykemanArmour = 60
+    wizardArmour = 20
+
+
+class NPCMagicResist:
+    knightMagicResist = 40
+    archerMagicResist = 40
+    pykemanMagicResist = 40
+    wizardMagicResist = 50
+
+
+class NPCMagic:
+    knightMagic = 30
+    archerMagic = 30
+    pykemanMagic = 30
+    wizardMagic = 100
+
+
+class NPCHunger:
+    knightHunger = 100
+    archerHunger = 100
+    pykemanHunger = 100
+    wizardHunger = 100
+
+class NPCMana:
+    knightMana = 100
+    archerMana = 100
+    pykemanMana = 100
+    wizardMana = 150
